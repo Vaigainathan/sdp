323d37e05d01b2d69fd24e8dfe5c367f3e67c8a6
Vaigainathan <136048029+Vaigainathan@users.noreply.github.com>
2024-11-08 14:16:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div classname="Name">
+      <h1>My name is Vaigainathan</h1>
+      <h2>I am 21 years old</h2>
+      <h2>I am currently at 3rd year of my college <br /> I am studying B.Tech CSE IOT <br/></h2>
+    </div>
+  );
+}
+
+export default App;
